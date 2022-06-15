e7f0a572caef4f68a8e16c86c09bfcbb8e90c8dc
Svetlana2512 <dellenn@mail.ru>
2022-06-15 23:35:21 +0400

    Initial commit

---
+++ src/main/java/com/geekbains/App.java
@@ -0,0 +1,13 @@
+package com.geekbains;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
